2080335de4a578f114f3557dd7cb234dfc804b37
Charlie-is-coding <k.doroszewski@gmail.com>
2021-03-09 12:40:48 +0100

    Added searching feature.

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ menu = """Please select one of the following options:
 4) Watch a movie.
 5) View watched movies.
 6) Add user to app.
-7) Exit.
+7) Search for a movie.
+8) Exit.
 
 Your selection: """
 
@@ -57,7 +58,16 @@ def prompt_show_watched_movies():
         print("That user has watched no movies yet")
 
 
-while (user_input := input(menu)) != "7":
+def prompt_search_movies():
+    search_term = input("Enter searching phrase:")
+    movies = database.search_movies(search_term)
+    if movies:
+        print_movie_list("Movies found", movies)
+    else:
+        print("No movies found")
+
+
+while (user_input := input(menu)) != "8":
     if user_input == "1":
         prompt_add_movie()
     elif user_input == "2":
@@ -72,5 +82,7 @@ while (user_input := input(menu)) != "7":
         prompt_show_watched_movies()
     elif user_input == "6":
         prompt_add_user()
+    elif user_input == "7":
+        prompt_search_movies()
     else:
         print("Invalid option, please try again.")
--- database.py
+++ database.py
@@ -30,7 +30,7 @@ SELECT_WATCHED_MOVIES = """SELECT movies.*
     WHERE users.username = ?;"""
 INSERT_WATCHED_MOVIE = "INSERT INTO watched (user_username, movie_id ) VALUES (?,?) "
 SET_MOVIE_WATCHED = "UPDATE movies SET watched = 1 WHERE title = ?;"
-
+SEARCH_MOVIES = "SELECT * from movies WHERE title LIKE ?;"
 
 connection = sqlite3.connect("data.db")
 
@@ -61,6 +61,13 @@ def get_movies(upcoming=False):
         else:
             cursor.execute(SELECT_ALL_MOVIES)
         return cursor.fetchall()
+
+
+def search_movies(search_term):
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(SEARCH_MOVIES, (f"%{search_term}%",))
+    return cursor.fetchall()
 
 
 def watch_movie(username, movie_id):
